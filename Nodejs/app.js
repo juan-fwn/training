adc42257868f058ced940c323a8fa4126e5c0c5e
Juan <juangiasl@gmail.com>
2023-12-01 12:11:12 -0300

    Sessions and Cookies
    Finish with this module

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -5,23 +5,44 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = `mongodb+srv://juangiasl:${process.env.MONGODB_PASSWORD}@cluster0.phy6ery.mongodb.net/shop`;
+
 const app = express();
+const store = MongoDBStore({
+  uri: MONGODB_URI,
+  connection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -31,19 +52,18 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    `mongodb+srv://juangiasl:${process.env.MONGODB_PASSWORD}@cluster0.phy6ery.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: "Max",
-          email: "max@test.com",
+          name: "Juan",
+          email: "juangiasl@test.com",
           cart: {
             items: [],
           },
