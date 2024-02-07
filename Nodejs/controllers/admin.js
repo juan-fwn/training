0b4f106f0ebf58a2a6159d0efb2c8122c4fc6bec
Juan <juangiasl@gmail.com>
2024-02-07 10:23:44 -0300

    Finished with Understanding Async Request

--- Nodejs/controllers/admin.js
+++ Nodejs/controllers/admin.js
@@ -161,8 +161,8 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => errorHandler(next, err));
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
 
   Product.findById(prodId)
     .then((product) => {
@@ -175,8 +175,9 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
-      console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
     })
-    .catch((err) => errorHandler(next, err));
+    .catch((err) =>
+      res.status(500).json({ message: "Deleting product failed." })
+    );
 };
